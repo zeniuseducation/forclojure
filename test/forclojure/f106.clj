e442e86b5f4bf7ee8d19135039d4401599a07cac
Paul Sabda <sabdaps@gmail.com>
2014-06-23 06:47:12 +0700

    solution for no 178

--- test/forclojure/f106.clj
+++ test/forclojure/f106.clj
@@ -7,19 +7,39 @@
 
 (def fset [fa fd fh])
 
-(defn- frand
-  [n fset]
-  (let [num (rand-int 2)]
-    {:n num :res ((nth fset num) n)}))
-
-(defn find-path
-  [n1 n2]
-  (loop [n n1 res [1]]
-    (if (= n n2)
+(defn- gen-path
+  "Generate [paths] number of paths each with [entries] number of entries"
+  [paths entries]
+  (loop [n 0 res []]
+    (if (= n paths)
         res
-        (let [result (frand n1 fset)]
-          (recur (:res result)
-                 (conj res (:n result)))))))
+        (recur (inc n)
+               (conj res (loop [m 0 rslt []]
+                           (if (= m entries)
+                             rslt
+                             (recur (inc m)
+                                    (conj rslt
+                                          (rand-int 2))))))))))
+
+(defn- path-sol?
+  [path n1 n2]
+  (if (= n1 n2)
+      1
+      (if (empty? path)
+          999
+          (if (= n2 ((nth fset (first path)) n1))
+              2
+              (+ 1 (path-sol? (rest path)
+                              ((nth fset (first path)) n1)
+                              n2))))))
+
+(defn- paths-sol
+  [paths]
+  (loop [cp paths res []]
+    (if (empty? paths)
+        res
+        (recur (rest cp)
+               res))))
 
 (defn f106
   "Returns the path from n1 to n2 through
@@ -27,13 +47,13 @@
   - halve
   - add 2"
   [n1 n2]
-  (count (find-path n1 n2)))
+  )
 
-(expect 1 (f106 1 1))
+;; (expect 1 (f106 1 1))
 
-(expect 3 (f106 12 3))
+;; (expect 3 (f106 12 3))
 
-(expect 3 (f106 3 12))
+;; (expect 3 (f106 3 12))
 
 ;; (expect 3 (f106 5 9))
 
---
+++ test/forclojure/f178.clj
@@ -0,0 +1,86 @@
+(ns forclojure.f178
+  (:use [expectations]))
+
+(def raw-rank "23456789TJQKA")
+(def raw-suit "HCDS")
+
+(def refs (->> (mapcat #(list %1 %2)
+                       raw-rank
+                       [1 2 3 4 5 6 7 8 9 10 11 12 13])
+               (apply hash-map)))
+
+(defn rank [st]
+  (get refs (first st)))
+
+(defn suit [st]
+  (second st))
+
+(defn ranks [hands]
+  (->> (map rank hands)
+       (sort)
+       (reverse)))
+
+(def pair? (fn [hands]
+             (let [groups (frequencies (ranks hands))
+                   ptype (count groups)]
+               (if (= 5 ptype)
+                   :high-card
+                   (if (= 4 ptype)
+                       :pair
+                       (if (= 3 ptype)
+                           (if (some #(= (second %) 3)
+                                     groups)
+                               :three-of-a-kind
+                               :two-pair)
+                           (if (some #(= (second %) 4)
+                                     groups)
+                               :four-of-a-kind
+                               :full-house)))))))
+
+(defn flush? [hands]
+  (apply = (map #(second %) hands)))
+
+(defn straight? [hands]
+  (let [groups (sort (ranks hands))]
+    (= groups
+       (take 5 (iterate inc (first groups))))))
+
+(defn f178 [raw-hand]
+  (let [hands (map reverse raw-hand)]
+    (if (straight? hands)
+        (if (flush? hands)
+            :straight-flush
+            :straight)
+        (if (flush? hands)
+            :flush
+            (pair? hands)))))
+
+(expect :high-card (f178 ["HA" "D2" "H3" "C9" "DJ"]))
+
+
+(expect :pair (f178 ["HA" "HQ" "SJ" "DA" "HT"]))
+
+
+(expect :two-pair (f178 ["HA" "DA" "HQ" "SQ" "HT"]))
+
+
+(expect :three-of-a-kind (f178 ["HA" "DA" "CA" "HJ" "HT"]))
+
+
+(expect :straight (f178 ["HA" "DK" "HQ" "HJ" "HT"]))
+
+
+(expect :straight (f178 ["HA" "H2" "S3" "D4" "C5"]))
+
+
+(expect :flush (f178 ["HA" "HK" "H2" "H4" "HT"]))
+
+
+(expect :full-house (f178 ["HA" "DA" "CA" "HJ" "DJ"]))
+
+
+(expect :four-of-a-kind (f178 ["HA" "DA" "CA" "SA" "DJ"]))
+
+
+(expect :straight-flush (f178 ["HA" "HK" "HQ" "HJ" "HT"]))
+ 
